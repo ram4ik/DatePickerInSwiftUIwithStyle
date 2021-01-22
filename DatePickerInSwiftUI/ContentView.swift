37d7aaceab86113c40df13484520637551bb9b0f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-22 15:31:19 +0200

    init

--- DatePickerInSwiftUI/ContentView.swift
+++ DatePickerInSwiftUI/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var birthDay = Date()
+    @State private var meetingDay = Date()
+    @State private var time = Date()
+    @State private var meetingWithTime = Date()
+    @State private var simple = Date()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form {
+                DatePicker("Birthday", selection: $birthDay, in: ...Date(), displayedComponents: .date)
+                DatePicker("Meeting", selection: $meetingDay, in: Date()..., displayedComponents: .date)
+                DatePicker("Time", selection: $time, in: Date()..., displayedComponents: .hourAndMinute)
+            }
+            Form {
+                DatePicker("Meeting & Time", selection: $meetingWithTime, in: Date()...)
+            }
+            Form {
+                DatePicker("", selection: $simple)
+                    .datePickerStyle(WheelDatePickerStyle())
+            }
+        }
     }
 }
 
